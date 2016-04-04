6541c36cdab5c336a47cc9ef77e5e423db1212ff
Pancham <pk2506@columbia.edu>
2016-04-03 23:37:54 -0400

    Update GraphAdjList.java

--- src/basic-graph/GraphAdjList.java
+++ src/basic-graph/GraphAdjList.java
@@ -1,1 +1,73 @@
 
+package basicgraph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+// Class that implements a directed graph by adjacency list representation
+
+public class GraphAdjList extends Graph {
+
+	private Map<Integer,ArrayList<Integer>> adjListsMap;
+
+	public GraphAdjList () {
+		adjListsMap = new HashMap<Integer,ArrayList<Integer>>();
+	}
+
+	public void implementAddVertex() {
+		int v = getNumVertices();
+		// System.out.println("Adding vertex "+v);
+		ArrayList<Integer> neighbors = new ArrayList<Integer>();
+		adjListsMap.put(v,  neighbors);
+	}
+	
+	public void implementAddEdge(int v, int w) {
+		(adjListsMap.get(v)).add(w);
+
+	}
+	
+
+	public List<Integer> getNeighbors(int v) {
+		return new ArrayList<Integer>(adjListsMap.get(v));
+	}
+
+	public List<Integer> getInNeighbors(int v) {
+		List<Integer> inNeighbors = new ArrayList<Integer>();
+		for (int u : adjListsMap.keySet()) {
+			//iterate through all edges in u's adjacency list and 
+			//add u to the inNeighbor list of v whenever an edge
+			//with startpoint u has endpoint v.
+			for (int w : adjListsMap.get(u)) {
+				if (v == w) {
+					inNeighbors.add(u);
+				}
+			}
+		}
+		return inNeighbors;
+	}
+
+
+	 public List<Integer> getDistance2(int v) {
+		 // To do
+		 return null;
+	}
+	
+	
+	public String adjacencyString() {
+		String s = "Adjacency list";
+		s += " (size " + getNumVertices() + "+" + getNumEdges() + " integers):";
+
+		for (int v : adjListsMap.keySet()) {
+			s += "\n\t"+v+": ";
+			for (int w : adjListsMap.get(v)) {
+				s += w+", ";
+			}
+		}
+		return s;
+	}
+
+
+}
